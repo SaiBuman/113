4f0ea4163bcf2cab5f8bf37c20455746cea761f4
Sai Buman <78092854+SaiBuman@users.noreply.github.com>
2022-05-19 12:22:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+function preload() {
+}
+
+function setup() {
+  canvas = createCanvas(500, 400);
+  canvas.position(400,200);
+  video = createCapture(VIDEO);
+  video.hide();
+}
+
+function draw() {
+  image(video, 80, 80, 350,250);
+  fill("red");
+  circle(50,50,100);
+  fill("green");
+  rect(100,30,300,40);
+  fill("red");
+  circle(450,50,100);
+  fill("green");
+  rect(30,100,40,200);
+  fill("red");
+  circle(50,350,100);
+  fill("green");
+  rect(100,330,300,40);
+  fill("green");
+  rect(430,100,40,200);
+  fill("red");
+  circle(450,350,100);
+}
+
+function take_snapshot(){    
+  save('student_name.png');
+}
+
+
